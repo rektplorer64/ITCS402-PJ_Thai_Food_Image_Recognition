9df94933dab9e9d5dab640d5ed85759dcba77295
rektplorer64 <tanawin2009@hotmail.com>
2020-09-30 14:30:31 +0700

    Removed normalizeMinMax.m because it is harder to implement for the case where a single image is being continuously inserted.

--- main_code/normalizeMinMax.m
+++
@@ -1,6 +0,0 @@
-function normalizedVector = normalizeMinMax(inputArray)
-%MINMAXNORMALIZATION Summary of this function goes here
-%   Detailed explanation goes here
-normalizedVector = (inputArray - min(inputArray)) ./ (max(inputArray) - min(inputArray));
-end
-
